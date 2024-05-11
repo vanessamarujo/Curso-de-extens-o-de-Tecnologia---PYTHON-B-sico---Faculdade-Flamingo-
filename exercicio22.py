a8aa54da00e06eb7f7397696bc8d1a5a499d23f4
Vanessa Marujo <vanessa_marujo@yahoo.com.br>
2024-05-11 11:33:29 -0300

    Add files via upload

---
+++ exercicio22.py
@@ -0,0 +1,11 @@
+# Curso Básico de PYTHON
+# Nome da desenvolvedora: Vanessa Marujo Nogueira
+# Versão 1.0
+# Exercício de lógica de programação com a linguagem de programação PYTHON
+# Exercício Saber se é  par ou ímpar
+
+numero =int(input("Digite um número para determinar se ele é par ou ímpar : "))
+if numero %2==0:
+    print("Este número é par ! ")
+else:
+    print("Este número é impar ! ")
