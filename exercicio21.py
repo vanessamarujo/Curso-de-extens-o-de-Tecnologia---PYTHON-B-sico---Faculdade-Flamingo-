ebdebcebc348c5530407d6d1307b8b3559eabea6
Vanessa Marujo <vanessa_marujo@yahoo.com.br>
2024-05-11 11:26:11 -0300

    Add files via upload

---
+++ exercicio21.py
@@ -0,0 +1,12 @@
+# Curso Básico de PYTHON
+# Nome da desenvolvedora: Vanessa Marujo Nogueira
+# Versão 1.0
+# Exercício de lógica de programação com a linguagem de programação PYTHON
+# Exercício Saber se é maior de idade ou não
+
+
+idade=int (input("Digite sua idade :"))
+if idade>=18:
+    print(" Você é maior de idade")
+else:
+    print("Você não é maior de idade")
