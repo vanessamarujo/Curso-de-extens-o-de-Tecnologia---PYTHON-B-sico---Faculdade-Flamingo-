e611f676a86d0774b1ab62e4562e40487c4d06ef
Vanessa Marujo <vanessa_marujo@yahoo.com.br>
2024-05-17 21:54:54 -0300

    Update exercicio27.py

--- exercicio27.py
+++ exercicio27.py
@@ -4,3 +4,8 @@
 # Exercício de lógica de programação com a linguagem de programação PYTHON
 # Exercício Digite Ler um valor e escrever a mensagem É MAIOR QUE 10! se o valor lido for maior que 10, caso contrário escrever NÃO É MAIOR QUE 10!
 
+numero=int(input("Digite um número"))
+if numero>= 10:
+ print("O valor é maior ou igual a 10!")
+else:
+ print("O valor é menor do que 10!")
