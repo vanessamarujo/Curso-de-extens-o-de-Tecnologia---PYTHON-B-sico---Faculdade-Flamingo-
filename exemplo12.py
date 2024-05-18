785f492e0abb2d1ff1b45f877ace19134e607577
Vanessa Marujo <vanessa_marujo@yahoo.com.br>
2024-05-18 12:55:53 -0300

    Add files via upload

---
+++ exemplo12.py
@@ -0,0 +1,11 @@
+# Curso Básico de PYTHON
+# Nome da desenvolvedora: Vanessa Marujo Nogueira
+# Versão 1.0
+# Exercício de lógica de programação com a linguagem de programação PYTHON
+# Exercício de Laços de repetição While
+
+contadora=1
+
+while contadora<11:
+  print(" Vanessa ")
+  contadora +=1
