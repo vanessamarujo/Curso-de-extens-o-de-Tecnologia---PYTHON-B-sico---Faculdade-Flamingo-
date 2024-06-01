13173d5a2cfce737fca5c53af625e5ce48127015
Vanessa Marujo <vanessa_marujo@yahoo.com.br>
2024-06-01 13:04:54 -0300

    Add files via upload

---
+++ exercicio54.py
@@ -0,0 +1,9 @@
+# Curso Básico de PYTHON
+# Nome da desenvolvedora: Vanessa Marujo Nogueira
+# Versão 1.0
+# Exercício de lógica de programação com a linguagem de programação PYTHON
+# Exercício Elaborar um programa que apresente no final o somatório dos valores pares existentes na faixa 
+# de 1 até 500
+
+acumuladora=0
+if acumuladora %==0
